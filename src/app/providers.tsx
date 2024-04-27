c3ab64dbd4ff685ba19d36e01a6af90f8f10df6d
matt haigh <matthaigh27@gmail.com>
2024-04-27 16:25:49 +1000

    service worker

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
@@ -15,6 +15,19 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         },
       }),
   );
+
+  useEffect(() => {
+    const registerServiceWorker = () => {
+      if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register("/service-worker.js");
+      }
+    };
+    window.addEventListener("load", registerServiceWorker);
+
+    return () => {
+      window.removeEventListener("load", registerServiceWorker);
+    };
+  }, []);
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
